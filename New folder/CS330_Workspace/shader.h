aecac1428b654def2c22c1a38947b6993ed0d128
RyanJMiller <126231840+RyanJMiller@users.noreply.github.com>
2024-08-24 04:39:33 -0500

    Delete New folder directory

--- New folder/CS330_Workspace/shader.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-
-#include <GL/glew.h>
-
-bool UCreateShaderProgram(const char* vtxShaderSource, const char* fragShaderSource, GLuint& programId);
-void UDestroyShaderProgram(GLuint programId);
--- New folder/CS330_Workspace/texture.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <GL/glew.h>
-
-bool UCreateTexture(const char* filename, GLuint& textureId);
-void UDestroyTexture(GLuint textureId);
